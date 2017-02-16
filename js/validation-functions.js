4eaf05002802b390f9bd6cbd1a73c173a625dcb0
arturoneill <mobius0919@gmail.com>
2017-02-16 15:40:10 -0500

    Update validation-functions.js

--- js/validation-functions.js
+++ js/validation-functions.js
@@ -88,29 +88,48 @@ $(document).ready(function() {
                     }
                 }
             }, 
-  dropoffName: {
-             message: 'Pick-up Name is not valid',
+  /*dropoffName: {
+             message: 'Drop-off Name is not valid',
                 validators: {
                     notEmpty: {
-                        message: 'Pick-up is required and cannot be empty'
+                        message: 'Drop-off is required and cannot be empty'
                     },
                     stringLength: {
                         min: 1,
                         max: 30,
-                        message: 'Pick-up name must be more than 1 and less than 30 characters long'
+                        message: 'Drop-off name must be more than 1 and less than 30 characters long'
                     },
                     regexp: {
                         regexp: /^(?!\s)[A-z ]+$/,
-                        message: 'Pick-up name can only accept alphabetical input'
+                        message: 'Drop-off name can only accept alphabetical input'
                     }
                 }
             },
+            */
             
             dropoffAddress: {
                 message: 'Address is not valid',
                 validators: {
                     notEmpty: {
                         message: 'Address is required and cannot be empty'
+                    }
+                }
+            }, 
+            
+            serviceLevel: {
+                message: 'Selection required',
+                validators: {
+                    notEmpty: {
+                        message: 'Selection required'
+                    }
+                }
+            }, 
+            
+             orderContents: {
+                message: 'Address is not valid',
+                validators: {
+                    notEmpty: {
+                        message: 'Order content is required and cannot be empty'
                     }
                 }
             }, 

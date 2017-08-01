79746d206aa7f32b61e925ff0c11574a98dbb3dd
arturoneill <mobius0919@gmail.com>
2017-08-01 14:26:07 -0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$('#MAPModal').on('shown.bs.modal', (function() {
+  var mapIsAdded = false;
+
+  return function() {
+    if (!mapIsAdded) {
+      $('.modal-body').html(' <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1coab886q4ow2cuO3ikcraTCJuT0" width="100%" height="400"></iframe>');
+
+      mapIsAdded = true;
+    }    
+  };
+})());

fc30c2f2e5be8c06957fba405b1c49e86bef0cab
arturoneill <mobius0919@gmail.com>
2017-08-13 16:05:37 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ $('#MAPModal').on('shown.bs.modal', (function() {
       $('.modal-map').html(' <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1D0l_yQnjPcvJHka8uFu0yRxBdek" width="100%" height="300"></iframe>');
 
       mapIsAdded = true;
+      
     }    
   };
 })());

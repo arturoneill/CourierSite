b4b6bd6b4176b6f4cc203048cddf36b6fd4e724f
arturoneill <mobius0919@gmail.com>
2017-09-25 13:38:52 -0400

    Update validation-functions.js

--- js/validation-functions.js
+++ js/validation-functions.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#service-form').bootstrapValidator({
+    $('#order-form').bootstrapValidator({
         //submitButtons: '#postForm',
         // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
         feedbackIcons: {

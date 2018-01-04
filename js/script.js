f56b5155fc9d116045424a871eb90e1cf280e5ad
arturoneill <mobius0919@gmail.com>
2018-01-03 23:12:21 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $('#MAPModal').on('shown.bs.modal', (function() {
     if (!mapIsAdded) {
       $('.modal-map').html(' <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1ednoEYxCJRQLIUdhxqD0z2Obl5V8qAMp" width="100%" height="300"></iframe>');
 
+      
       mapIsAdded = true;
       
       
